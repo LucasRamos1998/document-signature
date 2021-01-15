5a7bc86058b0c980c41e6b63f93ce13854d2ba7f
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-01-15 20:50:59 -0300

    feat: ensure SignUpController returns 400 if any required params is not provided

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -5,6 +5,10 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 export class SignupController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.name) return badRequest(new MissingParamError('name'))
+    if (!httpRequest.body.email) return badRequest(new MissingParamError('email'))
+    if (!httpRequest.body.cpf) return badRequest(new MissingParamError('cpf'))
+    if (!httpRequest.body.password) return badRequest(new MissingParamError('password'))
+    if (!httpRequest.body.passwordConfirmation) return badRequest(new MissingParamError('passwordConfirmation'))
     return null
   }
 }
