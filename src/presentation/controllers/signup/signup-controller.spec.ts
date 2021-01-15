bc85a9e483b397b368205bebeb1f349f9152ff33
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-01-15 20:51:30 -0300

    test: ensure SignUpController returns 400 if any required params is not provided

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -15,4 +15,56 @@ describe('Signup Controller', () => {
     })
     expect(httpResponse).toEqual(badRequest(new MissingParamError('name')))
   })
+
+  test('Should return 400 if cpf is not provided', async () => {
+    const sut = new SignupController()
+    const httpResponse = await sut.handle({
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        email: 'any_email@mail.com'
+      }
+    })
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('cpf')))
+  })
+
+  test('Should return 400 if email is not provided', async () => {
+    const sut = new SignupController()
+    const httpResponse = await sut.handle({
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        cpf: 'any_cpf'
+      }
+    })
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should return 400 if password is not provided', async () => {
+    const sut = new SignupController()
+    const httpResponse = await sut.handle({
+      body: {
+        name: 'any_name',
+        cpf: 'any_cpf',
+        passwordConfirmation: 'any_password',
+        email: 'any_email@mail.com'
+      }
+    })
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+
+  test('Should return 400 if password is not provided', async () => {
+    const sut = new SignupController()
+    const httpResponse = await sut.handle({
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        cpf: 'any_cpf',
+        email: 'any_email@mail.com'
+      }
+    })
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('passwordConfirmation')))
+  })
 })
