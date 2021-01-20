8e9998b3bcbbb4ab32702a580c7879c7fc3aca4b
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-01-19 21:07:41 -0300

    feat: ensure SignupController returns 400 if invalid email is provided

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,13 +1,32 @@
+import { AddAccount } from '../../../domain/usecases/add-account'
+import { EmailInUseError } from '../../errors/email-in-use-error'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest } from '../../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
 
 export class SignupController implements Controller {
+  constructor (
+    private readonly addAccount: AddAccount,
+    private readonly emailValidator: EmailValidator
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['name', 'email', 'cpf', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
     }
+    const { name, cpf, email, password } = httpRequest.body
+
+    const isValid = this.emailValidator.isValid(email)
+
+    if (!isValid) return badRequest(new EmailInUseError())
+
+    await this.addAccount.add({
+      name,
+      cpf,
+      email,
+      password
+    })
     return null
   }
 }
