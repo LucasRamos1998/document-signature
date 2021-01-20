8ba57ff491cc7fddd60d8a517ab8c9f07b0726af
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-01-20 19:29:53 -0300

    feat: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -2,7 +2,7 @@ import { AddAccount } from '../../../domain/usecases/add-account'
 import { CompareFieldsError } from '../../errors/compare-fields-error'
 import { EmailInUseError } from '../../errors/email-in-use-error'
 import { MissingParamError } from '../../errors/missing-param-error'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../../protocols'
 
 export class SignupController implements Controller {
@@ -12,23 +12,27 @@ export class SignupController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['name', 'email', 'cpf', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'cpf', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
+      }
+      const { name, cpf, email, password, passwordConfirmation } = httpRequest.body
+
+      const isValid = this.emailValidator.isValid(email)
+
+      if (!isValid) return badRequest(new EmailInUseError())
+      if (password !== passwordConfirmation) return badRequest(new CompareFieldsError('password', 'passwordConfirmation'))
+
+      await this.addAccount.add({
+        name,
+        cpf,
+        email,
+        password
+      })
+      return null
+    } catch (err) {
+      return serverError()
     }
-    const { name, cpf, email, password, passwordConfirmation } = httpRequest.body
-
-    const isValid = this.emailValidator.isValid(email)
-
-    if (!isValid) return badRequest(new EmailInUseError())
-    if (password !== passwordConfirmation) return badRequest(new CompareFieldsError('password', 'passwordConfirmation'))
-
-    await this.addAccount.add({
-      name,
-      cpf,
-      email,
-      password
-    })
-    return null
   }
 }
