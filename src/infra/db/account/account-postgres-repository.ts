f5ced41dd563e5725cef0bfa05ad5eb3a1356c0e
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-02-24 20:33:16 -0300

    feat: ensure AccountPostgresRepository returns an account on add success

--- src/infra/db/account/account-postgres-repository.ts
+++ src/infra/db/account/account-postgres-repository.ts
@@ -1,13 +1,14 @@
-import { AddAccountRepository } from "../../../data/protocols/db/account/add-account-repository";
-import { AccountModel } from "../../../domain/models/account";
-import { AddAccountParams } from "../../../domain/usecases/add-account";
+import { AddAccountRepository } from '../../../data/protocols/db/account/add-account-repository'
+import { AccountModel } from '../../../domain/models/account'
+import { AddAccountParams } from '../../../domain/usecases/add-account'
 import { Account } from '../type-orm/entities/Account'
-import connection from '../type-orm/postgresql/helpers/pg-typeorm-helper'
+import { PgHelper } from '../type-orm/postgresql/helpers/postgres-helper'
 
 export class AccountPostgresRepository implements AddAccountRepository {
-    async add (accountData: AddAccountParams): Promise<AccountModel> {
-        const accounts = connection.client.getRepository(Account)
-        const newAccount = await accounts.create(accountData).save()
-        return newAccount
-    }
+  async add (accountData: AddAccountParams): Promise<AccountModel> {
+    const accountRepository = await PgHelper.getRepository(Account)
+    const newAccount = await accountRepository.create(accountData)
+    await accountRepository.save(newAccount)
+    return newAccount
+  }
 }
