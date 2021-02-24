c36d1077a821a60c6aae2ca98c9556e2d63f28d6
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-02-24 20:34:20 -0300

    test: ensure AccountPostgresRepository returns an account on add success

--- src/infra/db/account/account-postgres-repository.spec.ts
+++ src/infra/db/account/account-postgres-repository.spec.ts
@@ -1,36 +1,34 @@
-import connection from '../type-orm/postgresql/helpers/pg-typeorm-helper'
 import { AccountPostgresRepository } from './account-postgres-repository'
+import { PgHelper } from '../type-orm/postgresql/helpers/postgres-helper'
+import { Account } from '../type-orm/entities/Account'
 
 describe('Account Postgres Repository', () => {
-    const client = connection
+  beforeAll(async () => {
+    await PgHelper.connect()
+  })
 
-    beforeAll(async () => {
-        await client.create()
-    })
+  afterAll(async () => {
+    await PgHelper.disconnect()
+  })
 
-    afterAll(async () => {
-        await client.close()
-    })
-    
-    beforeEach(async () => {
-        await client.clear()
-    })
-    
-    const makeSut = (): AccountPostgresRepository => {
-        return new AccountPostgresRepository()
-    }
-    
+  afterEach(async () => {
+    const accountRepository = await PgHelper.getRepository(Account)
+    await accountRepository.clear()
+  })
 
-    test('Should return an account on add success', async () => {
-        const sut = makeSut()
-        const account = await sut.add({
-            name: 'any_name',
-            cpf: 'any_cpf',
-            email: 'any_email@mail.com',
-            password: 'any_password'
-        })
-        expect(account).toBeTruthy()
-        expect(account.id).toBeTruthy()
-        expect(account.name).toBe('any_nameASDASDAS')
+  const makeSut = (): AccountPostgresRepository => {
+    return new AccountPostgresRepository()
+  }
+  test('Should return an account on add success', async () => {
+    const sut = makeSut()
+    const account = await sut.add({
+      name: 'any_name',
+      cpf: 'any_cpf',
+      email: 'any_email@mail.com',
+      password: 'any_password'
     })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+  })
 })
