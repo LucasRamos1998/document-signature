ec66c41dd60c08232fac3358d7a9d6ec2765c224
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-02-26 20:59:36 -0300

    feat: ensure load returns an account if cpf or email exists

--- src/infra/db/account/account-postgres-repository.ts
+++ src/infra/db/account/account-postgres-repository.ts
@@ -1,14 +1,24 @@
 import { AddAccountRepository } from '../../../data/protocols/db/account/add-account-repository'
+import { LoadAccountByEmailOrCpf, LoadAccountByEmailOrCpfParams } from '../../../data/usecases/add-account/db-add-account-protocols'
 import { AccountModel } from '../../../domain/models/account'
 import { AddAccountParams } from '../../../domain/usecases/add-account'
 import { Account } from '../type-orm/entities/Account'
 import { PgHelper } from '../type-orm/postgresql/helpers/postgres-helper'
 
-export class AccountPostgresRepository implements AddAccountRepository {
+export class AccountPostgresRepository implements AddAccountRepository, LoadAccountByEmailOrCpf {
   async add (accountData: AddAccountParams): Promise<AccountModel> {
     const accountRepository = await PgHelper.getRepository(Account)
     const newAccount = await accountRepository.create(accountData)
     await accountRepository.save(newAccount)
     return newAccount
+  }
+
+  async load (params: LoadAccountByEmailOrCpfParams): Promise<AccountModel> {
+    const { cpf, email } = params
+    const accountRepository = await PgHelper.getRepository(Account)
+    const account = await accountRepository.find({
+      where: [{ cpf }, { email }]
+    })
+    return account.length ? account[0] : null
   }
 }
