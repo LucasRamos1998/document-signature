6a45bf9aa2e5f05bba63209ec817d90d9111f6a5
LucasRamos1998 <lucasramosxs@hotmail.com>
2021-03-02 21:25:49 -0300

    test: ensure SignupRoute return 200 on success

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,12 +1,22 @@
 import request from 'supertest'
+import { Account } from '../../infra/db/type-orm/entities/Account'
+import { PgHelper } from '../../infra/db/type-orm/postgresql/helpers/postgres-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await PgHelper.connect()
+  })
+  beforeEach(async () => {
+    const accountRepository = await PgHelper.getRepository(Account)
+    await accountRepository.clear()
+  })
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
-        name: 'Mango',
+        name: 'Maga0',
+        cpf: '11111111122',
         email: 'manomango@gmail.com',
         password: '123123',
         passwordConfirmation: '123123'
